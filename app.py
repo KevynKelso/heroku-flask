bc168ffe6ca6c4ea746c71b6319aae80884af7b5
Kelso <kkelso@swatchgroup.net>
2021-10-15 16:56:17 -0600

    t

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import paho.mqtt.client as mqtt
 import json
+import time
 
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
+DELAY = 1  # longer delays may improve performance and use less data
 USERNAME = "kkelso"
 PASSWORD = "hiveMQ!23"
 # HOST = "74a454df0d3ed456fbfb6a3d1ed57b14f.s1.eu.hivemq.cloud"
@@ -16,13 +18,12 @@ topics = 'Warehouse Truck Site'.split()
 debug_topic = 'debug'
 
 serial_numbers = '104608418437344 10643107158240 110032962132192 126796219488480 146084900837600 156135141087456 16321053923552 163612645595360 205673293879520 216187357042912 220254691072224 251869962115296 252239312525536 257779820337376 259283058890976 260099102677216 268160756291808 270394139285728 277077125175520 279203100432608 29811562977504 33664115087584 40819547379936 41291993782496 43280563640544 44388681980128 50556238239968 50813936277728 53064499140832 6000247511264 60043804219616 63106132678880 70523557976288 97324137126112'.split()
-mac_addresses = []
+mac_addresses = ['e0:18:9f:09:7d:36']
 # {"DeviceName":"N/A","DeviceMAC":"E0:18:9F:09:7D:36","DeviceRSSI":-50}
 devices = {'warehouse': 0, 'truck': 0, 'site': 0}
 warehouse_beacons = {}
 truck_beacons = {}
 site_beacons = {}
-msg_count = 0
 
 
 def sn_to_bytes(sn: int) -> str:
@@ -40,9 +41,10 @@ def init_beacons():
         if mac not in mac_addresses:
             mac_addresses.append(mac)
 
-        warehouse_beacons[mac] = 0
-        truck_beacons[mac] = 0
-        site_beacons[mac] = 0
+    for mac in mac_addresses:
+        warehouse_beacons[mac] = 999
+        truck_beacons[mac] = 999
+        site_beacons[mac] = 999
 
 
 def on_connect(client, userdata, flags, rc):
@@ -105,6 +107,7 @@ def on_topic_msg(topic, beacons, client, userdata, msg):
     client.publish(debug_topic, f'{topic} updated')
 
     update_devices()
+    time.sleep(DELAY)
 
 
 def on_warehouse_msg(client, userdata, msg):
