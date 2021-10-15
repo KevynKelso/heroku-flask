152376fbbfc0cdb2906849f83d6744bf869a0650
Kelso <kkelso@swatchgroup.net>
2021-10-15 14:52:58 -0600

    test

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
-USERNAME="kkelso"
-PASSWORD="hiveMQ!23"
-HOST="74a454df0d3ed456fbfb6a3d1ed57b14f.s1.eu.hivemq.cloud"
-PORT=8883
+USERNAME = "kkelso"
+PASSWORD = "hiveMQ!23"
+HOST = "74a454df0d3ed456fbfb6a3d1ed57b14f.s1.eu.hivemq.cloud"
+PORT = 8883
 
 topics = 'Warehouse Truck Site'.split()
 debug_topic = 'debug'
@@ -16,6 +16,7 @@ debug_topic = 'debug'
 # {"DeviceName":"N/A","DeviceMAC":"E0:18:9F:09:7D:36","DeviceRSSI":-50}
 data = {}
 devices = {'warehouse': 0, 'truck': 0, 'site': 0}
+
 
 def on_connect(client, userdata, flags, rc):
     for topic in topics:
@@ -33,6 +34,7 @@ def on_message(client, userdata, msg):
 @app.route("/")
 def home():
     return render_template("home.html", name="home", data=data, devices=devices)
+
 
 client = mqtt.Client()
 client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
