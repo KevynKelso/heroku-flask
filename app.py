270c3e967f0d487901d5d40301057216284b885f
Kelso <kkelso@swatchgroup.net>
2021-10-15 15:48:02 -0600

    tsest

--- app.py
+++ app.py
@@ -16,14 +16,37 @@ topics = 'Warehouse Truck Site'.split()
 debug_topic = 'debug'
 
 serial_numbers = '104608418437344 10643107158240 110032962132192 126796219488480 146084900837600 156135141087456 16321053923552 163612645595360 205673293879520 216187357042912 220254691072224 251869962115296 252239312525536 257779820337376 259283058890976 260099102677216 268160756291808 270394139285728 277077125175520 279203100432608 29811562977504 33664115087584 40819547379936 41291993782496 43280563640544 44388681980128 50556238239968 50813936277728 53064499140832 6000247511264 60043804219616 63106132678880 70523557976288 97324137126112'.split()
-
 mac_addresses = []
 # {"DeviceName":"N/A","DeviceMAC":"E0:18:9F:09:7D:36","DeviceRSSI":-50}
-data = {}
+# is DeviceName = Warehouse | truck | site ?
 devices = {'warehouse': 0, 'truck': 0, 'site': 0}
+warehouse_beacons = {}
+truck_beacons = {}
+site_beacons = {}
+
+
+def sn_to_bytes(sn: int) -> str:
+    temp = hex(int.from_bytes(sn.to_bytes(6, 'little'), 'big'))[2:]
+    returnValue = temp[:2]
+    for i in range(1, int(len(temp)/2)):
+        returnValue += f':{temp[2*i:2*i+2]}'
+
+    return returnValue
+
+
+def init_beacons():
+    for sn in serial_numbers:
+        mac = sn_to_bytes(sn)
+        if mac not in mac_addresses:
+            mac_addresses.append(mac)
+
+        warehouse_beacons[mac] = 0
+        truck_beacons[mac] = 0
+        site_beacons[mac] = 0
 
 
 def on_connect(client, userdata, flags, rc):
+    init_beacons()
     for topic in topics:
         client.subscribe(topic)
         client.publish(debug_topic, f'Subscribed to {topic}')
@@ -39,8 +62,10 @@ def validate_data(data):
     if 'DeviceRSSI' not in data.keys():
         return False
 
-    if data['DeviceMAC'] not in mac_addresses:
+    if data['DeviceMAC'] not in warehouse_beacons.keys():
         return False
+
+    return True
 
 
 def on_message(client, userdata, msg):
@@ -51,10 +76,22 @@ def on_message(client, userdata, msg):
         client.publish(debug_topic, 'invalid data')
         return
 
+    if data['DeviceName'].lower() == 'warehouse':
+        warehouse_beacons[data['DeviceMAC']] = abs(int(data['DeviceRSSI']))
+        client.publish(debug_topic, 'warehouse updated')
+
+    if data['DeviceName'].lower() == 'truck':
+        truck_beacons[data['DeviceMAC']] = abs(int(data['DeviceRSSI']))
+        client.publish(debug_topic, 'truck updated')
+
+    if data['DeviceName'].lower() == 'site':
+        site_beacons[data['DeviceMAC']] = abs(int(data['DeviceRSSI']))
+        client.publish(debug_topic, 'site updated')
+
 
 @app.route("/")
 def home():
-    return render_template("home.html", name="home", data=data, devices=devices)
+    return render_template("home.html", name="home", devices=devices, beacons=warehouse_beacons)
 
 
 client = mqtt.Client()
